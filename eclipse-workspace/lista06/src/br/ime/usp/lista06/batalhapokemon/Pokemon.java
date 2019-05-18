3e27002a7dd5183d6abca00e307b4dffc01717f5
JHKroger00 <50018245+JHKroger00@users.noreply.github.com>
2019-05-18 12:47:25 -0300

    Update Pokemon.java

--- eclipse-workspace/lista06/src/br/ime/usp/lista06/batalhapokemon/Pokemon.java
+++ eclipse-workspace/lista06/src/br/ime/usp/lista06/batalhapokemon/Pokemon.java
@@ -1,7 +1,7 @@
 /*********************************************************/
-/** 						                            **/
+/** 						        **/
 /** Autores: Joao Henrique de A. Kroger  NUSP: 10770109 **/
-/**			 Bruno Macedo Sanches        NUSP: 10770263 **/
+/**	     Bruno Macedo Sanches        NUSP: 10770263 **/
 /**                                                     **/
 /** Professor: Marcelo Finger                           **/
 /** Lista 06 - Exercicio 01                             **/
